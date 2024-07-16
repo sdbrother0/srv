ca950dbc4560d3c93e9e93b16614251fd3e1d672
sergei demin <s.demin@x5.ru>
2024-07-16 14:17:07 +0300

    init

--- src/main/java/com/example/demo/controller/Test1Controller.java
+++ src/main/java/com/example/demo/controller/Test1Controller.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.SaveDto;
-import com.example.demo.dto.Table2Dto;
 import com.example.demo.dto.meta.MetaData;
 import com.example.demo.entity.Test1Entity;
 import com.example.demo.repository.Test1Repository;
