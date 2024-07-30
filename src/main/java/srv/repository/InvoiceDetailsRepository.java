9115b9a2cc9f6419ce3b07ac5544bcb150423dbf
sergei demin <s.demin@x5.ru>
2024-07-30 17:22:53 +0300

    init

--- src/main/java/srv/repository/InvoiceDetailsRepository.java
+++ src/main/java/srv/repository/InvoiceDetailsRepository.java
@@ -1,10 +1,16 @@
 package srv.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import srv.entity.InvoiceDetailsEntity;
-import srv.entity.InvoiceEntity;
 
 public interface InvoiceDetailsRepository extends JpaRepository<InvoiceDetailsEntity, Long>, JpaSpecificationExecutor<InvoiceDetailsEntity> {
 
+    @Override
+    @EntityGraph(attributePaths = {"product.id", "invoice.customer"})
+    Page<InvoiceDetailsEntity> findAll(Specification<InvoiceDetailsEntity> spec, Pageable pageable);
 }
