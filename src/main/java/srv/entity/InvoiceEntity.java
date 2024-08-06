54bf75b8951f4a8118681b4c996402538e5b57f1
sergei demin <s.demin@x5.ru>
2024-08-06 14:28:40 +0300

    init

--- src/main/java/srv/entity/InvoiceEntity.java
+++ src/main/java/srv/entity/InvoiceEntity.java
@@ -37,7 +37,7 @@ public class InvoiceEntity {
     @Formula(value="(select coalesce(sum((inv_d.tax + inv_d.price) * inv_d.quantity), 0) FROM invoice_details inv_d WHERE inv_d.invoice_id = id)")
     private Long total;
 
-    @Formula(value="(select coalesce(sum(inv_d.tax), 0) FROM invoice_details inv_d WHERE inv_d.invoice_id = id)")
+    @Formula(value="(select coalesce(sum(inv_d.tax * inv_d.quantity), 0) FROM invoice_details inv_d WHERE inv_d.invoice_id = id)")
     private Long taxTotal;
 
 }
---
+++ src/main/resources/db/changelog/init/examples.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+--changeset sdbrother0:examples
+INSERT INTO product(name, price) VALUES('Product 1', '100');
+INSERT INTO product(name, price) VALUES('Product 2', '150');
+INSERT INTO product(name, price) VALUES('Product 3', '400');
+
+INSERT INTO customer(email, first_name, last_name) VALUES('cust1@cust.com', 'Cust1', 'Cust');
+INSERT INTO customer(email, first_name, last_name) VALUES('cust2@cust.com', 'Cust2', 'Cust');
+
+INSERT INTO invoice(created, customer_id) VALUES(now(), 1);
+INSERT INTO invoice_details(invoice_id, product_id, quantity, price, tax) VALUES (1, 1, 1, 100, 20);
+INSERT INTO invoice_details(invoice_id, product_id, quantity, price, tax) VALUES (1, 2, 2, 150, 30);
