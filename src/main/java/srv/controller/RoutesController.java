fea49d1614813957d3818737845c0681ff0cc364
sergei demin <s.demin@x5.ru>
2024-11-15 15:12:20 +0300

    full meta

--- src/main/java/srv/controller/RoutesController.java
+++ src/main/java/srv/controller/RoutesController.java
@@ -4,30 +4,30 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import srv.dto.meta.Menu;
 import srv.dto.meta.Route;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @Log4j2
 public class RoutesController {
 
-    private static final String DATA_URL = "/routes";
+    private static final String DATA_URL = "/menu";
 
     @GetMapping("/meta" + DATA_URL)
-    public Map<String, List<Route>> getRoutes() {
+    public List<Menu> getRoutes() {
         log.info("get routes");
-        Map<String, List<Route>> routes = new HashMap<>();
         List<Route> invoiceRoutes = new ArrayList<>();
         invoiceRoutes.add(Route.builder().title("Product").path("product").metaUrl("/meta/product").build());
         invoiceRoutes.add(Route.builder().title("Customer").path("customer").metaUrl("/meta/customer").build());
         invoiceRoutes.add(Route.builder().title("Invoice").path("invoice").metaUrl("/meta/invoice").build());
-        routes.put("Invoice example", invoiceRoutes);
-        return routes;
+
+        List<Menu> menuList = new ArrayList<>();
+        menuList.add(Menu.builder().title("Invoice example").routes(invoiceRoutes).build());
+        return menuList;
     }
 
 }
