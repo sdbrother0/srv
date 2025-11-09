b5964ddbe78c2eda4bf3725d6468ed72603e2f8b
sd <demin_sv@mail.ru>
2025-11-09 23:32:28 +0300

    CI/CD deploy

--- src/test/java/srv/SrvApplicationTests.java
+++ src/test/java/srv/SrvApplicationTests.java
@@ -1,12 +1,14 @@
 package srv;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SrvApplicationTests {
 
+    @DisplayName("test!!!!!!!!!!!!!")
     @Test
     void contextLoads() {
         Assertions.assertTrue(true);
