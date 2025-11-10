18ad43cb331dd8740fcfcea9560b2fcf514992ea
sd <demin_sv@mail.ru>
2025-11-10 10:14:29 +0300

    example metadata fix

--- src/main/java/srv/domains/example/ExampleController.java
+++ src/main/java/srv/domains/example/ExampleController.java
@@ -1,6 +1,5 @@
 package srv.domains.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import srv.dto.meta.MetaData;
 
 import java.util.List;
 import java.util.UUID;
--- src/main/java/srv/domains/example/ExampleService.java
+++ src/main/java/srv/domains/example/ExampleService.java
@@ -1,6 +1,5 @@
 package srv.domains.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import srv.dto.meta.MetaData;
 import srv.specification.SimpleLikeSpecification;
 
 import java.util.ArrayList;
