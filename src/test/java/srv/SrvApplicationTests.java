4f8bd121883a2b1692383f705e4d4b024b79638d
sd <demin_sv@mail.ru>
2025-11-10 12:16:11 +0300

    example metadata fix

--- src/test/java/srv/SrvApplicationTests.java
+++ src/test/java/srv/SrvApplicationTests.java
@@ -1,17 +1,32 @@
 package srv;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SrvApplicationTests {
 
-    @DisplayName("test!!!!!!!!!!!!!")
+    @Autowired
+    private MockMvc mockMvc;
+
+    @DisplayName("Get menu test")
     @Test
-    void contextLoads() {
-        Assertions.assertTrue(true);
+    void contextLoads() throws Exception {
+        mockMvc.perform(get("/menu")
+                .contentType(String.valueOf(MediaType.APPLICATION_JSON)))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].title").value("Invoice example"))
+            .andExpect(jsonPath("$[0].routes").isArray());
     }
 
 }
