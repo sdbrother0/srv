73094f66f78e82bd007170021835cdd382387c83
S.Demin <S.Demin@x5.ru>
2026-03-31 15:59:02 +0200

    fix test

--- src/test/java/srv/SrvApplicationTests.java
+++ src/test/java/srv/SrvApplicationTests.java
@@ -2,11 +2,12 @@ package srv;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
